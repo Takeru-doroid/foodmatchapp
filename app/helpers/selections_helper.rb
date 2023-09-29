ea3c43dee65b1b9535648ff882554158db7dee88
Takeru-doroid <post8zntyn@gmail.com>
2023-09-29 22:03:26 +0900

    新しいcategoryと食材追加、それに伴い組み合わせ処理の微修正

--- app/helpers/selections_helper.rb
+++ app/helpers/selections_helper.rb
@@ -2,7 +2,7 @@ module SelectionsHelper
   def find_display_dish(category_ids, select_ingredients)
     all_category_ids = category_ids&.map(&:to_i)
     if all_category_ids.present?
-      duplicate_categories = all_category_ids.tally.select{ |k, v| v > 1 }.map(&:first)
+      duplicate_categories = all_category_ids.tally.select { |k, v| v > 1 }.map(&:first)
       if duplicate_categories.present?
         if duplicate_categories.include?(3)
           if select_ingredients.pluck(:name).include?("上ケモノ肉")
@@ -11,18 +11,18 @@ module SelectionsHelper
             Dish.find_by(name: "山海焼き")
           else
             pluck_dish_id = CategoryDish.where(category_id: duplicate_categories).pluck(:dish_id)
-            display_dish = CategoryDish.where(dish_id: pluck_dish_id)
-                                      .group(:dish_id)
-                                      .count.select{ |k, v| v == 1 }
-                                      .map(&:first)
+            display_dish = CategoryDish.where(dish_id: pluck_dish_id).
+              group(:dish_id).
+              count.select { |k, v| v == 1 }.
+              map(&:first)
             Dish.find_by(id: display_dish)
           end
         else
           pluck_dish_id = CategoryDish.where(category_id: duplicate_categories).pluck(:dish_id)
-          display_dish = CategoryDish.where(dish_id: pluck_dish_id)
-                                    .group(:dish_id)
-                                    .count.select{ |k, v| v == 1 }
-                                    .map(&:first)
+          display_dish = CategoryDish.where(dish_id: pluck_dish_id).
+            group(:dish_id).
+            count.select { |k, v| v == 1 }.
+            map(&:first)
           Dish.find_by(id: display_dish)
         end
       else
@@ -30,20 +30,22 @@ module SelectionsHelper
           Dish.find_by(name: "上山海焼き")
         elsif all_category_ids.include?(2) && all_category_ids.include?(3)
           Dish.find_by(name: "山海焼き")
+        elsif select_ingredients.pluck(:name).include?("ガンバリ草") && select_ingredients.count == 1
+          Dish.find_by(name: "丸ごと焼き")
         elsif all_category_ids.count > 1
-          display_dish = Dish.joins(:category_dishes)
-                            .where(category_dishes: { category_id: all_category_ids })
-                            .pluck(:dish_id)
-                            .tally
-                            .select{ |k, v| v > 1 }
-                            .map(&:first)
+          display_dish = Dish.joins(:category_dishes).
+            where(category_dishes: { category_id: all_category_ids }).
+            pluck(:dish_id).
+            tally.
+            select { |k, v| v > 1 }.
+            map(&:first)
           Dish.find_by(id: display_dish)
         else
           pluck_dish_id = CategoryDish.where(category_id: all_category_ids).pluck(:dish_id)
-          display_dish = CategoryDish.where(dish_id: pluck_dish_id)
-                                    .group(:dish_id)
-                                    .count.select{ |k, v| v == 1 }
-                                    .map(&:first)
+          display_dish = CategoryDish.where(dish_id: pluck_dish_id).
+            group(:dish_id).
+            count.select { |k, v| v == 1 }.
+            map(&:first)
           Dish.find_by(id: display_dish)
         end
       end
